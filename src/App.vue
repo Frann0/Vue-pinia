bf302d4e06bebf4a9b2576519988ead1af7f7e1a
Mike Hovedskov <deengo113@gmail.com>
2024-03-11 16:48:17 +0100

    Fixed issue with clean up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 import Navbar from "./components/navbar/navbar.vue";
 import { useCartStore } from "@/stores/cart";
 import { useProductStore } from "@/stores/products";
